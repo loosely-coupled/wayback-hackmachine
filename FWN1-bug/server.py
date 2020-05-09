f282beb38e9b9dac394d8b4e3b02a324cf7579ba
Masande Mtintsilana <masande.mtintsilana@mwrinfosecurity.com>
2020-05-09 16:43:52 +0200

    add server auth

--- FWN1-bug/server.py
+++ FWN1-bug/server.py
@@ -1,16 +1,23 @@
-# based off:https://pymotw.com/3/http.server/
+""" 
+This is an example of the FWN/1 protocol vulnerability discovered by Thomas Lopatic in Firewall-1.
+Description of the vulnerability comes from the book "The art of software security assessments"
+Note: This only aims to re-create the core vulnerability and does not resemble the actual protocol
+
+The scaffolding code for the server is based off https://pymotw.com/3/http.server/
+"""
 import http.server
 import socketserver
 from http.server import BaseHTTPRequestHandler
 from http.cookies import SimpleCookie
 from urllib import parse
 from random import randint
+import hashlib
 
 PORT = 8000
 
 class FWN1RequestHandler(BaseHTTPRequestHandler):
 
-    SECRET = randint(1000000000,9999999999)
+    SECRET = str(randint(1000000000,9999999999))
     sessions = []
 
     def client_auth(self):
@@ -25,15 +32,9 @@ class FWN1RequestHandler(BaseHTTPRequestHandler):
 
     def server_auth(self):
 
-        ## read random value
-
-        ## read local secret
-
-        ## hash secret  + random value
-
-        ## compare hashes
-
-        pass
+        r1 = randint(1000000000,9999999999) 
+        key = str(r1) + FWN1RequestHandler.SECRET       
+        return (r1, hashlib.sha256(key.encode('utf-8')).hexdigest())
 
     def store_session(self, sessionid):
         sessions.append[sessionid]
@@ -51,8 +52,14 @@ class FWN1RequestHandler(BaseHTTPRequestHandler):
     
     def do_GET(self):
         print(FWN1RequestHandler.SECRET)
+        
         if self.path.startswith("/serverauth"):
-            return self.server_auth()
+            iv, fhash = self.server_auth()
+            response = "iv:%s\r\nhash:%s" % (iv, fhash)
+            
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(response.encode('utf-8')) 
         
         elif self.path.startswith("/clientauth"):
             return self.client_auth()
@@ -67,6 +74,7 @@ class FWN1RequestHandler(BaseHTTPRequestHandler):
                 self.end_headers
                 self.wfile.write("'fwn1protocol' cookie not found. Please authenticate\n".encode('utf-8'))
                 return
+
             if self.check_session(cookies["fwn1protocol"].value) == False:
                 self.send_response(404)
                 self.end_headers  
